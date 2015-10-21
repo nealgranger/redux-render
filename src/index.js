d97cfe9aa9d531922129d612529cd17a38c434bc
Neal Granger <neal@nealg.com>
2015-10-20 20:04:55 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,60 +1,120 @@
-import { renderToString } from 'react-dom/server';
+import { renderToString as render } from 'react-dom/server';
+import { isFSA } from 'flux-standard-action';
 import isPromise from 'is-promise';
 
-function wait(element : Element) {
-
-  const store = element.props.store;
-
-  return new Promise((resolve, reject) => {
-    function done() {
-      if (--pending <= 0) {
-        store.dispatch = store._dispatch;
-        delete store._dispatch;
-        resolve({ waited, markup });
-      }
-    }
-
-    const promises = [ ];
-    let pending = 0;
-    let waited = false;
-    let markup = '';
-
-    store._dispatch = store.dispatch;
-    store.dispatch = (...args) => {
-      ++pending;
-      const result = store._dispatch(...args);
-      if (isPromise(result)) {
-        waited = true;
-        promises.push(result);
-      } else {
-        done();
-      }
-    }
-    markup = renderToString(element);
-    promises.forEach(promise => promise.then(done, done));
-  });
-
+const ActionTypes = {
+  PERFORM_ACTION: 'PERFORM_ACTION',
+  RESET_PROMISES: 'RESET_PROMISES',
+  INIT: '@@redux/INIT'
 }
 
-function render(element : Element, count = 0) : Promise {
-  return wait(element).then(({ waited, markup }) => {
-    if (!waited || count > 5) {
-      return Promise.resolve({ markup, count });
+export default function renderToString(element, store, count = 5) {
+  console.log('render ', count);
+
+  const markup = render(element);
+
+  return Promise.all(store.renderStore.getState().promises).then(actions => {
+    store.renderStore.dispatch({ type: ActionTypes.RESET_PROMISES });
+
+    // TODO: Check action payloads for redirects and errors.
+
+    if (!actions.length || --count === 0 ) {
+      const status = 200;
+      const state = store.getState();
+      return { markup, state, status };
     }
-    return render(element, count + 1);
+
+    return this.renderToString(element, count)
   });
 }
 
-/**
- * Render a redux container.
- * @param {Element} container Redux container to render.
- * @returns {Promise} Promise containing render result.
- */
-export default function(container : Element) : Promise {
-  return new Promise((resolve, reject) => {
-    if (!container || !container.props || !container.props.store) {
-      return reject(new Error('Container must have a valid redux store.'));
+function appendPromise(promises, action) {
+  if (!isFSA(action)) {
+    if (isPromise(action)) {
+      return promises.concat([action]);
     }
-    return render(container.props.store)
-  });
+  }
+
+  if (isPromise(action.payload)) {
+    return promises.concat([action]);
+  }
+
+  return promises;
+}
+
+function liftReducer(reducer, initialState) {
+  const initialLiftedState = {
+    storeState: initialState,
+    promises: []
+  };
+
+  return function liftedReducer(liftedState = initialLiftedState, liftedAction) {
+    let {
+      storeState,
+      promises
+    } = liftedState;
+
+    switch(liftedAction.type) {
+    case ActionTypes.INIT:
+      storeState = reducer(storeState, liftedAction);
+      break;
+    case ActionTypes.PERFORM_ACTION:
+      promises = appendPromise(promises, liftedAction.action);
+      storeState = reducer(storeState, liftedAction.action);
+      break;
+    case ActionTypes.RESET_PROMISES:
+      promises = []
+      break;
+    }
+
+    return  {
+      storeState,
+      promises
+    }
+  }
+}
+
+
+function liftAction(action) {
+  const liftedAction = {
+    type: ActionTypes.PERFORM_ACTION,
+    action
+  };
+  return liftedAction;
+}
+
+
+function unliftState(liftedState) {
+  return liftedState.storeState;
+}
+
+function unliftStore(liftedStore, reducer) {
+  return {
+    ...liftedStore,
+    renderStore: liftedStore,
+    dispatch(action) {
+      liftedStore.dispatch(liftAction(action));
+      return action;
+    },
+    getState() {
+      const state = unliftState(liftedStore.getState());
+      return state;
+    },
+    getReducer() {
+      return reducer;
+    },
+    replaceReducer(nextReducer) {
+      liftedStore.replaceReducer(liftReducer(nextReducer));
+    }
+  }
+}
+
+
+export function renderMiddleware() {
+  return next => (reducer, initialState) => {
+    const liftedReducer = liftReducer(reducer, initialState);
+    const liftedStore = next(liftedReducer);
+    const store = unliftStore(liftedStore, reducer);
+    return store;
+  }
 }
