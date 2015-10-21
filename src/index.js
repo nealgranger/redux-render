2ad6674b5f3a345a2e24d65250e70ad52618eb85
Neal Granger <neal@nealg.com>
2015-10-21 00:04:11 -0700

    Don't lift actions

--- src/index.js
+++ src/index.js
@@ -2,20 +2,13 @@ import { renderToString as render } from 'react-dom/server';
 import { isFSA } from 'flux-standard-action';
 import isPromise from 'is-promise';
 
-const ActionTypes = {
-  PERFORM_ACTION: 'PERFORM_ACTION',
-  RESET_PROMISES: 'RESET_PROMISES',
-  INIT: '@@redux/INIT'
-}
-
 export default function renderToString(element, store, count = 5) {
   console.log('render ', count);
 
   const markup = render(element);
 
   return Promise.all(store.renderStore.getState().promises).then(actions => {
-    store.renderStore.dispatch({ type: ActionTypes.RESET_PROMISES });
-
+    // TODO: Reset the list of promises.
     // TODO: Check action payloads for redirects and errors.
 
     if (!actions.length || --count === 0 ) {
@@ -48,24 +41,14 @@ function liftReducer(reducer, initialState) {
     promises: []
   };
 
-  return function liftedReducer(liftedState = initialLiftedState, liftedAction) {
+  return function liftedReducer(liftedState = initialLiftedState, action) {
     let {
       storeState,
       promises
     } = liftedState;
-
-    switch(liftedAction.type) {
-    case ActionTypes.INIT:
-      storeState = reducer(storeState, liftedAction);
-      break;
-    case ActionTypes.PERFORM_ACTION:
-      promises = appendPromise(promises, liftedAction.action);
-      storeState = reducer(storeState, liftedAction.action);
-      break;
-    case ActionTypes.RESET_PROMISES:
-      promises = []
-      break;
-    }
+    
+    promises = appendPromise(promises, action);
+    storeState = reducer(storeState, action);
 
     return  {
       storeState,
@@ -73,16 +56,6 @@ function liftReducer(reducer, initialState) {
     }
   }
 }
-
-
-function liftAction(action) {
-  const liftedAction = {
-    type: ActionTypes.PERFORM_ACTION,
-    action
-  };
-  return liftedAction;
-}
-
 
 function unliftState(liftedState) {
   return liftedState.storeState;
@@ -92,10 +65,6 @@ function unliftStore(liftedStore, reducer) {
   return {
     ...liftedStore,
     renderStore: liftedStore,
-    dispatch(action) {
-      liftedStore.dispatch(liftAction(action));
-      return action;
-    },
     getState() {
       const state = unliftState(liftedStore.getState());
       return state;
@@ -108,7 +77,6 @@ function unliftStore(liftedStore, reducer) {
     }
   }
 }
-
 
 export function renderMiddleware() {
   return next => (reducer, initialState) => {
